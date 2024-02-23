c139436ec441693d90f85e4e31d29abf5cd026c8
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-02-23 09:10:56 +0200

    initialize app with our database

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+    databaseURL: "https://realtime-database-93b45-default-rtdb.europe-west1.firebasedatabase.app";
+}
+
+const app = initializeApp(appSettings);

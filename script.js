b11891639ddcc8d0d0763a957353492e6165d42e
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-02-23 09:08:55 +0200

    import getDatabase function from Firebase database library

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
